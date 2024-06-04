a594f4b8ff5fb529769cdaa2908a1e2b35a1aa9e
Thatonerookiedev <shatoriag01@gmail.com>
2024-06-04 10:46:40 -0500

    try 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig =  {};
+const nextConfig =  {
+  distDir: "build", //this line will tell the build to create a file with this name
+};;
 
 export default nextConfig;
